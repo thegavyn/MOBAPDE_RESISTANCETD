83e12e235b04c880875cd24ab539196070a524db
Gavin Sanchez <mark_gavin_sanchez@dlsu.edu.ph>
2017-11-05 21:03:54 +0800

    Working Version for Game Loop
    Prints the FPS now

---
+++ src/main/java/com/example/markgavin/resistancetd/GamePanel.java
@@ -0,0 +1,71 @@
+package com.example.markgavin.resistancetd;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+/**
+ * Created by Mark Gavin on 11/2/2017.
+ */
+
+public class GamePanel extends SurfaceView  implements SurfaceHolder.Callback{
+
+    private MainThread thread;
+
+    public GamePanel(Context context)
+    {
+        super(context);
+
+        getHolder().addCallback(this);
+
+        thread = new MainThread(getHolder(), this);
+
+        setFocusable(true);
+    }
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height)
+    {
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder)
+    {
+        thread = new MainThread(getHolder(), this);
+        thread.setRunning(true);
+        thread.start();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder){
+        boolean retry = true;
+
+        while(retry == true)
+        {
+            try{
+                thread.setRunning(false);
+                thread.join();
+                retry = false;
+            }catch(Exception e) {e.printStackTrace();retry = true;}
+            retry = false;
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        return super.onTouchEvent(event);
+    }
+
+    public void update()
+    {
+
+    }
+
+    @Override
+    public void draw(Canvas canvas){
+        super.draw(canvas);
+    }
+}

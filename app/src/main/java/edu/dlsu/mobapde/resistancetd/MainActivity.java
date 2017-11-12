4437824121e102501d8c4bd499d3bdd988709975
alvinalvord <jasper_pillejera@dlsu.edu.ph>
2017-11-12 22:03:12 +0800

    added trix's opening video
    The current video is a placeholder. It will be trimmed and it should have a narration.

--- app/src/main/java/edu/dlsu/mobapde/resistancetd/MainActivity.java
+++ app/src/main/java/edu/dlsu/mobapde/resistancetd/MainActivity.java
@@ -1,7 +1,10 @@
 package edu.dlsu.mobapde.resistancetd;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -13,5 +16,13 @@ public class MainActivity extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences dsp = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+
+        if (!dsp.contains("resistance_td_opening_viewed")) {
+            Intent i = new Intent (getBaseContext(), TrixVideoActivity.class);
+            startActivity(i);
+            finish();
+        }
     }
 }

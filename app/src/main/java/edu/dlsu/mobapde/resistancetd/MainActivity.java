688256244837c1108620531992d886e4a4afa81b
alvinalvord <jasper_pillejera@dlsu.edu.ph>
2017-11-12 20:26:28 +0800

    Created Main Menu
    images and icons and other color schemes subject to change

--- app/src/main/java/edu/dlsu/mobapde/resistancetd/MainActivity.java
+++ app/src/main/java/edu/dlsu/mobapde/resistancetd/MainActivity.java
@@ -1,13 +1,17 @@
 package edu.dlsu.mobapde.resistancetd;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
     }
 }

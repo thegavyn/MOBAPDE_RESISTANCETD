ab4293d3b8f80204d41ac1008918d17363e45d75
Jolene Valenzuela <jolene_valenzuela@dlsu.edu.ph>
2017-11-15 16:28:36 +0800

    Rectangles are finally falling, again

--- app/src/main/java/edu/dlsu/mobapde/resistancetd/MainActivity.java
+++ app/src/main/java/edu/dlsu/mobapde/resistancetd/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.DisplayMetrics;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.Window;
@@ -26,13 +27,19 @@ public class MainActivity extends Activity {
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-		// remove title
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_main);
-
 		// shared preferences
 		dsp = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
 		checkOpeningViewed();
+
+		// from Gavin's code
+		DisplayMetrics dm = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(dm);
+		Constants.SCREEN_WIDTH = dm.widthPixels;
+		Constants.SCREEN_HEIGHT = dm.heightPixels;
+
+		// remove title
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_main);
 
 		// initialize elements and add listeners
 		initAttributes();

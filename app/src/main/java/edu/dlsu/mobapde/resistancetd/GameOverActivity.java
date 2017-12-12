fef5919d4bdf425b4da6b9a0af774dcd9b9342a6
Jolene Valenzuela <jolene_valenzuela@dlsu.edu.ph>
2017-12-12 12:35:51 +0800

    Added GameOverActivity xml and other stuff

---
+++ app/src/main/java/edu/dlsu/mobapde/resistancetd/GameOverActivity.java
@@ -0,0 +1,13 @@
+package edu.dlsu.mobapde.resistancetd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameOverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_over);
+    }
+}
